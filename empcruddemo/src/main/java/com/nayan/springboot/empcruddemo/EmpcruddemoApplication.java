226fdc5200d682a6806fa429a18b560b96627fe5
Nayan Doshi <nayandoshi1008@gmail.com>
2024-06-14 14:04:41 +0530

    Initialize spring boot project

---
+++ empcruddemo/src/main/java/com/nayan/springboot/empcruddemo/EmpcruddemoApplication.java
@@ -0,0 +1,13 @@
+package com.nayan.springboot.empcruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpcruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpcruddemoApplication.class, args);
+	}
+
+}
